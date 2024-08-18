baaddcb3efae5f89a522bfb41df7015699bb91e1
NickShein <ddiiggyykkookkyy@gmail.com>
2024-08-18 23:44:08 +0300

    fixing display issues

--- src/app/components/error/error.component.ts
+++ src/app/components/error/error.component.ts
@@ -1,17 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
   styleUrls: ['./error.component.scss'],
 })
-export class ErrorComponent implements OnInit {
-  constructor(private router: Router) {}
-
-  ngOnInit(): void {
-    setTimeout(() => {
-      this.router.navigate(['/']);
-    }, 4000);
-  }
+export class ErrorComponent {
 }
